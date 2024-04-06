1c72cf6e7a0e13675fe6b5bdbe7c5f357d0eead8
Luan Felipe Marmentini <luanf2003@gmail.com>
2024-04-06 12:56:26 -0300

    feat: Add QrCode Scanner

---
+++ components/qr-code-scanner/index.js
@@ -0,0 +1,54 @@
+import { View, Text } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera/next";
+import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+export default function QrCodeScanner() {
+  const [permissiton, requestPermission] = useCameraPermissions();
+
+  const onQrScanned = (scanningResult) => {
+    alert(scanningResult.data)
+  };
+
+  return (
+    <View style={styles.container}>
+      <CameraView
+        style={styles.camera}
+        facing="back"
+        barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
+        onBarcodeScanned={onQrScanned}
+      >
+        <View></View>
+      </CameraView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    backgroundColor: "#AFD6C4",
+  },
+  camera: {
+    width: 500,
+    height: "70%",
+    backgroundColor: "yellow",
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
