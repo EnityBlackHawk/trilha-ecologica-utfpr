79ef405c227bbb539785bd902c4c7581ea7e614c
lucasgabwinter <lucasgabwinter@gmail.com>
2024-04-09 12:28:42 -0300

    adicao do cartao sobre a flora

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import Router from "./router";
+import Router from './router';
+import FloraCard from './pages/FloraCard';
 
 export default function App() {
   return <Router />;
